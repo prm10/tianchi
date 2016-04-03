820fbad6df83cd8224ec21454daad65af993031a
prm14 <prm@datrain.cn>
2016-04-03 22:08:31 +0800

    改变了模型输入

--- my_arma.m
+++ my_arma.m
@@ -1,17 +1,19 @@
-function [theta,bias,S,L]=my_arma(y,sta,m)
-%从y的第sta+1个开始，预测至结尾,模型长度为m+1; sta>=m
-n=length(y)-sta;
-y0=y(sta+1-m:sta);
-T=y(sta+1:end);
+function [theta,bias,S,L]=my_arma(y,m,n)
+%y长N,利用y的第1~m，预测第m+1~m+n个；到最后，用第N-n-m+1~N-n个，预测N-n+1~N，模型长度为m+1
 theta=normrnd(0,0.1,[m,1]);%zeros(m+1,1);
-bias=normrnd(0,0.1,[1]);
+bias=normrnd(0,0.1,1);
 loops=2e4;
 L=zeros(loops,1);
 lr=1e-2;
+N=length(y);
+batch=N-n-m+1;
+
 for i1=1:loops
+    i2=mod(i1,batch);
+    y0=y(i2+1:i2+m);
+    T=y(i2+m+1:i2+m+n);
     S=my_predict(theta,bias,n,y0);
     [grad_theta,grad_bias,L(i1)]=my_gradient(S,T,y0,theta);
     theta=theta-lr*grad_theta;
     bias=bias-lr*grad_bias;
-%     disp(strcat(num2str(i1),': ',num2str(L)));
 end
